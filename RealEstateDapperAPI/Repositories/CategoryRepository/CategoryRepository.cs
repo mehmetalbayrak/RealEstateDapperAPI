3d4c15190ccdd5daba99c854593a69ff71119c96
Mehmet Albayrak <mehmet__albayrak@outlook.com>
2023-08-06 14:43:33 +0300

    added category add operation

--- RealEstateDapperAPI/Repositories/CategoryRepository/CategoryRepository.cs
+++ RealEstateDapperAPI/Repositories/CategoryRepository/CategoryRepository.cs
@@ -13,6 +13,20 @@ namespace RealEstateDapperAPI.Repositories.CategoryRepository
             _context = context;
         }
 
+        public async void CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            string query = "Insert Into Category(CategoryName,CategoryStatus) Values(@categoryName,@categoryStatus)";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@categoryName", createCategoryDto.CategoryName);
+            parameters.Add("@categoryStatus", true);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
         public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
         {
             string query = "Select * from Category";
