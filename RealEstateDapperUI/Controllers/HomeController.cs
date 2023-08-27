b0dc5998ae2648b3da62ef0238df8d9e807661b2
Mehmet Albayrak <mehmet__albayrak@outlook.com>
2023-08-27 17:07:42 +0300

    done api consume

--- RealEstateDapperUI/Controllers/HomeController.cs
+++ RealEstateDapperUI/Controllers/HomeController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstateDapperUI.Dtos.ProductDtos;
 
 namespace RealEstateDapperUI.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44302/api/Products/GetAllProductsWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
